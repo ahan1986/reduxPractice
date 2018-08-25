46c9f76aa8e1bee7b4f8a828808ec11f0addd5bc
Handrew <andrew.a.han@gmail.com>
2018-08-25 11:21:03 -0400

    setting up redux

--- reduxpractice/src/App.js
+++ reduxpractice/src/App.js
@@ -1,21 +1,45 @@
 import React, { Component } from 'react';
+import { createStore } from 'redux';
 
 
-
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
           
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+//           <h1 className="App-title">Welcome to React</h1>
+//         </header>
+//         <p className="App-intro">
+//           To get started, edit <code>src/App.js</code> and save to reload.
+//         </p>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+
+
+const initialStore = {
+    result: 1,
+    lastValues: []
+};
+
+const reducer = ( state , action ) => {
+  switch (action.type) {
+    case "ADD":
+      break;
+    case "SUBTRACT":
+      break;
   }
+  return state;
 }
 
-export default App;
+const store = createStore(reducer, 1);
+
+store.dispatch({
+    type: 'ADD',
+    payload: 100
+})
--- reduxpractice/src/index.js
+++ reduxpractice/src/index.js
@@ -3,6 +3,40 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore } from 'redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<App />, document.getElementById('root'));
+// registerServiceWorker();
+
+
+const initialStore = {
+    result: 1,
+    lastValues: []
+};
+
+const reducer = ( state , action ) => {
+  switch (action.type) {
+    case "ADD":
+        state = state + action.payload;
+        break;
+    case "SUBTRACT":
+      break;
+  }
+  return state;
+}
+
+const store = createStore(reducer, 1);
+
+store.subscribe(() => {
+    console.log("Store updated!", store.getState());
+});
+
+store.dispatch({
+    type: 'ADD',
+    payload: 100
+});
+
+store.dispatch({
+    type: 'ADD',
+    payload: 10
+});
