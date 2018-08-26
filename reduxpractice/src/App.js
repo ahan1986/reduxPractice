c0599ac882a9583655042c1c6f7e11fff3670b37
Handrew <andrew.a.han@gmail.com>
2018-08-26 00:10:39 -0400

    connecting redux with react

--- reduxpractice/src/App.js
+++ reduxpractice/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Main } from './components/Main';
 import { User } from './components/User';
+import { connect } from 'tls';
 
 
 class App extends React.Component {
@@ -24,12 +25,31 @@ class App extends React.Component {
     return (
       <div className='container'>
         <Main changeUsername={this.changeUsername} />
-        <User username={this.state.username} />
+        {/* redux uses props and populates it. From the userReducer in index.js, there's a property named "name" and "age" we can now access */}
+        <User username={this.props.user1.name} />
       </div>
     );
   }
 }
 
-export default App;
+// this function allows redux to connect with components but you also need another function called connect() to connects react.js with redux
+const mapStateToProps = (state) => {
+  return {
+    user1: state.userReducerGuy,
+    math1: state.mathReducerGuy
+  }
+}
 
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setName: (name) => {
+      dispatch({
+        type: "SET_NAME",
+        payload: name
+      })
+    }
+  }
+}
 
+//don't need to export default the App in this class. We can now do it this way below
+export default connect(mapStateToProps, mapDispatchToProps)(App)
--- reduxpractice/src/index.js
+++ reduxpractice/src/index.js
@@ -1,81 +1,82 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import App from './App';
+// import store from './store';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-
-
-// import { createStore, combineReducers, applyMiddleware } from 'redux';
-// import { createLogger } from 'redux-logger';
-
-// const mathReducer = ( state = {
-//     result: 1,
-//     lastValues: []
-// } , action ) => {
-//   switch (action.type) {
-//     case "ADD":
-//         state = {
-//             ...state,
-//             result: state.result + action.payload,
-//             lastValues: [...state.lastValues, action.payload]
-//         }
+const mathReducer = ( state = {
+    result: 1,
+    lastValues: []
+} , action ) => {
+  switch (action.type) {
+    case "ADD":
+        state = {
+            ...state,
+            result: state.result + action.payload,
+            lastValues: [...state.lastValues, action.payload]
+        }
         
-//         break;
-//     case "SUBTRACT":
-//         state = {
-//             result: state.result,
-//             lastValues: [],
-//             result: state.result + action.payload,
-//             lastValues: [...state.lastValues, action.payload]
-//         };
+        break;
+    case "SUBTRACT":
+        state = {
+            result: state.result,
+            lastValues: [],
+            result: state.result + action.payload,
+            lastValues: [...state.lastValues, action.payload]
+        };
         
-//         break;
-//   }
-//   return state;
-// }
+        break;
+  }
+  return state;
+}
 
-// // another reducer for users
-// const userReducer = ( state = {
-//     name: 'Andrew',
-//     age: 31
-// } , action ) => {
-//     switch (action.type) {
-//       case "SET_NAME":
-//           state = {
-//               ...state,
-//               name: action.payload
-//           }
+// another reducer for users
+const userReducer = ( state = {
+    name: 'Andrew',
+    age: 31
+} , action ) => {
+    switch (action.type) {
+      case "SET_NAME":
+          state = {
+              ...state,
+              name: action.payload
+          }
           
-//           break;
-//       case "SET_AGE":
-//           state = {
-//               ...state,
-//               name: action.payload
-//           };
+          break;
+      case "SET_AGE":
+          state = {
+              ...state,
+              name: action.payload
+          };
           
-//           break;
-//     }
-//     return state;
-//   }
+          break;
+    }
+    return state;
+  }
 
-// //creating a middleware. (store) function uses (next) function which passes to the (action) function then it has a function body that we can do anything and use the three arguments of the function used.
-// const myLogger = (store) => (next) => (action) => {
-//     console.log("Logged Action: ", action);
-//     //execute next(action) which is provided and we need this to continue the action to get to the reducer
-//     next(action);
-// }
+//creating a middleware. (store) function uses (next) function which passes to the (action) function then it has a function body that we can do anything and use the three arguments of the function used.
+const myLogger = (store) => (next) => (action) => {
+    console.log("Logged Action: ", action);
+    //execute next(action) which is provided and we need this to continue the action to get to the reducer
+    next(action);
+}
 
-// const store = createStore(combineReducers({
-//         mathReducer,
-//         userReducer
-//     }), 
-//         {}, 
-//         applyMiddleware(createLogger())
-//     );
+const store = createStore(combineReducers({
+        mathReducerGuy: mathReducer,
+        userReducerGuy: userReducer
+    }), 
+        {}, 
+        applyMiddleware(createLogger())
+    );
 
-// store.subscribe(() => {
-//     console.log("Store updated!", store.getState());
-// });
+store.subscribe(() => {
+    console.log("Store updated!", store.getState());
+});
+
+
+// the provider only provides the store. You need to connect and set up another function to connect react with redux and it's states
+ReactDOM.render(<Provider store= {store}><App /></Provider>, document.getElementById('root'));
